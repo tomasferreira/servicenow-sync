d37c8485152f2e8789ad4ae51e5e0975a03bc775
tomasferreira <tomasferreira@gmail.com>
2019-04-03 21:26:47 +0200

    added request lib

--- extension.js
+++ extension.js
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const https = require('https');
+
+
+async function syncFile(){
+  vscode.window.showErrorMessage('oh no');
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +23,12 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.syncFile', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+    vscode.window.showInformationMessage('Hello World!');
+    syncFile();
 	});
 
 	context.subscriptions.push(disposable);
